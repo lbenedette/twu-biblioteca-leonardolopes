9f0686f381bdca08497dd35049964cf6967885cc
Leonardo Lopes <blopesleo@outlook.com>
2020-04-08 21:08:32 -0300

    Added a list of books (String) to Library and printed a list after a welcome message

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,7 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+
 public class Library {
+    private List<String> books;
+
+    public Library(List<String> books) {
+        this.books = books;
+    }
+
     public void run() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+
+        for (String book : books) {
+            System.out.println(book);
+        }
     }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
@@ -28,7 +30,31 @@ public class LibraryTest {
 
     @Test
     public void shouldPrintWelcomeMessageOnRun() {
-        Library library = new Library();
+        List<String> books = new ArrayList<>();
+        Library library = new Library(books);
+
+        library.run();
+
+        String firstLine = outContent.toString().split("\n")[0];
+        assertThat(firstLine, is("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"));
+    }
+
+    @Test
+    public void shouldPrintABookListAfterWelcomeMessage() {
+        List<String> books = new ArrayList<>();
+        Library library = new Library(books);
+
+        books.add("The Fellowship Of The Ring");
+        library.run();
+
+        String secondLine = outContent.toString().split("\n")[1];
+        assertThat(secondLine, is("The Fellowship Of The Ring"));
+    }
+
+    @Test
+    public void shouldPrintNothingAfterWelcomeMessageIfBooksIsEmpty() {
+        List<String> books = new ArrayList<>();
+        Library library = new Library(books);
 
         library.run();
 
