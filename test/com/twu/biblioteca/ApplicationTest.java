d232b69484fe6bb1841a62cd640f73bca7cc7c1c
Leonardo Lopes <blopesleo@outlook.com>
2020-04-09 12:35:32 -0300

    Created a new class Application to handle states, print and user inputs

---
+++ test/com/twu/biblioteca/ApplicationTest.java
@@ -0,0 +1,37 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
+public class ApplicationTest {
+    private PrintStream sysOut;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() throws Exception {
+        sysOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void setDown() throws Exception {
+        System.setOut(sysOut);
+    }
+
+    @Test
+    public void shouldPrintWelcomeMessageOnRun() {
+        Application app = new Application();
+
+        app.run();
+
+        assertThat(outContent.toString().split("\n")[0], is("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"));
+    }
+}
