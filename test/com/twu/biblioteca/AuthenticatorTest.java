9d1d697a393eb2ffef7285edc2d83ef854a14dfe
Leonardo Lopes <blopesleo@outlook.com>
2020-04-22 14:44:47 -0300

    Refactored AuthenticatorTest

--- test/com/twu/biblioteca/AuthenticatorTest.java
+++ test/com/twu/biblioteca/AuthenticatorTest.java
@@ -2,6 +2,7 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.exceptions.InvalidPasswordException;
 import com.twu.biblioteca.exceptions.UserNotFoundException;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,31 +12,31 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AuthenticatorTest {
+    private User user = new User("123-1234", "verycomplicatedpassword");
+    private List<User> users;
+    private Authenticator authenticator;
+
+    @Before
+    public void setUp() {
+        users = new ArrayList<>();
+        authenticator = new Authenticator(users);
+    }
 
     @Test
     public void returnUserWithLibraryNumberAndPasswordTest() {
-        User user = new User("123-1234", "verycomplicatedpassword");
-        List<User> users = new ArrayList<>();
         users.add(user);
-        Authenticator authenticator = new Authenticator(users);
 
         assertThat(authenticator.authenticate("123-1234", "verycomplicatedpassword"), is(user));
     }
 
     @Test(expected = UserNotFoundException.class)
     public void throwExceptionWhenUserIsNotFoundTest() {
-        List<User> users = new ArrayList<>();
-        Authenticator authenticator = new Authenticator(users);
-
         authenticator.authenticate("123-1234", "verycomplicatedpassword");
     }
 
     @Test(expected = InvalidPasswordException.class)
     public void throwExceptionWhenPasswordIsIncorrectTest() {
-        User user = new User("123-1234", "verycomplicatedpassword");
-        List<User> users = new ArrayList<>();
         users.add(user);
-        Authenticator authenticator = new Authenticator(users);
 
         authenticator.authenticate("123-1234", "incorrectpassword");
     }
